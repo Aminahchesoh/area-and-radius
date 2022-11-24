bc82a9e8095a66c832ecf5a5c3d2342abfc0b73c
Aminah <98732130+Aminahchesoh@users.noreply.github.com>
2022-11-24 09:43:45 +0700

    Create area.py

---
+++ area.py
@@ -0,0 +1,8 @@
+import math
+
+area = int(input("Enter the area: "))
+if area > 0:
+  radius = math.sqrt(area / math.pi)
+  print ("The radius is", radius)
+else:  
+  print ("Error: the area must be a positive number")
